9db3066c11a4ccd711499745e5bdc2b05ca57fd4
AceLorenzG <acelorenzgallo@gmail.com>
2023-01-23 10:37:21 +0800

    Updates
    collage

---
+++ Hello/Hello.java
@@ -0,0 +1,7 @@
+public class Hello
+{
+    public static void main(String[] args)
+    {
+        System.out.println("I think that i shall never see,\na poem as lovely as tree,\na tree whose hungry mouth is pressed\nAgainst the Earth's sweet flowering breast.");
+    }
+}
